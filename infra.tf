a03080d381e68082a8b1adc569bbbae1c97a0200
Linn Pe Htut <linnpehtut@Linns-MacBook-Pro.local>
2024-09-29 11:17:12 +0800

    igw and subnet

--- infra.tf
+++ infra.tf
@@ -5,3 +5,22 @@ resource "aws_vpc" "rancher_vpc" {
     Name = "${var.prefix}-rancher-vpc"
   }
 }
+
+resource "aws_internet_gateway" "rancher_gateway" {
+  vpc_id = aws_vpc.rancher_vpc.id
+
+  tags = {
+    Name = "${var.prefix}-rancher-gateway"
+  }
+}
+
+resource "aws_subnet" "rancher_subnet" {
+  vpc_id = aws_vpc.rancher_vpc.id
+
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = var.aws_zone
+
+  tags = {
+    Name = "${var.prefix}-rancher-subnet"
+  }
+}
