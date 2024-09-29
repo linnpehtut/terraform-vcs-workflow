3557cdf1e790857906457a5723bbfd1291ac0ba8
Linn Pe Htut <linnpehtut@Linns-MacBook-Pro.local>
2024-09-29 11:28:05 +0800

    undo comment out test

--- infra.tf
+++ infra.tf
@@ -1,26 +1,26 @@
-# resource "aws_vpc" "rancher_vpc" {
-#   cidr_block           = "10.0.0.0/16"
-#   enable_dns_hostnames = true
-#   tags = {
-#     Name = "${var.prefix}-rancher-vpc"
-#   }
-# }
+resource "aws_vpc" "rancher_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.prefix}-rancher-vpc"
+  }
+}
 
-# resource "aws_internet_gateway" "rancher_gateway" {
-#   vpc_id = aws_vpc.rancher_vpc.id
+resource "aws_internet_gateway" "rancher_gateway" {
+  vpc_id = aws_vpc.rancher_vpc.id
 
-#   tags = {
-#     Name = "${var.prefix}-rancher-gateway"
-#   }
-# }
+  tags = {
+    Name = "${var.prefix}-rancher-gateway"
+  }
+}
 
-# resource "aws_subnet" "rancher_subnet" {
-#   vpc_id = aws_vpc.rancher_vpc.id
+resource "aws_subnet" "rancher_subnet" {
+  vpc_id = aws_vpc.rancher_vpc.id
 
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = var.aws_zone
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.aws_zone
 
-#   tags = {
-#     Name = "${var.prefix}-rancher-subnet"
-#   }
-# }
+  tags = {
+    Name = "${var.prefix}-rancher-subnet"
+  }
+}
